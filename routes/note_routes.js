931fd7b17e2ad445190417959c0e7d3e108d2e98
Freddy Lau <freddylaudesign@gmail.com>
2024-01-23 19:03:32 -0500

    creating routes for api

--- routes/note_routes.js
+++ routes/note_routes.js
@@ -1,18 +1,31 @@
 const router = require('express').Router()
 const path = require('path')
-const { getNotes, saveNotes } = require('../server')
+// const { getNotes, saveNotes } = require('../public')
 
-// Create route: GET /notes should return the notes.html file
-router.get('/notes', async (requestOb, responseObj,) => {
-    responseObj.sendFile(path.join(__dirname, '../public/notes.html'))
-},
-    const notes = await getNotes()
-)
 
-// Create route: GET * should return the index.html file
+// Create route: GET / should return index
 router.get('/', (requestObj, responseObj) => {
     responseObj.sendFile(path.join(__dirname, '../public/index.html'))
 }
 )
+
+// Create route: GET /notes should return the notes.html file
+router.get('/notes', (requestOb, responseObj,) => {
+    responseObj.sendFile(path.join(__dirname, '../public/notes.html'))
+    // const notes = getNotes()
+
+}
+)
+
+
+// Create route: GET /api/notes should read the db.json file and return all saved notes as JSON
+router.get('')
+
+
+
+//Wildcard Route: GET * should return the index.html file
+router.get('*', (requestObj, responseObj) => {
+    responseObj.sendFile(path.join(__dirname, '../public/index.html'))
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,17 +7,17 @@ const PORT = 3334
 
 app.use(express.static('./public'))
 
-// Get notes data from JSON
-async function getNotes() {
-    const notes = await fs.promises.readFile('./db/db.json', 'utf8');
-    console.log('hello', notes)
-    return JSON.parse(notes)
-}
+// // Get notes data from JSON
+// async function getNotes() {
+//     const notes = await fs.promises.readFile('./db/db.json', 'utf8');
+//     console.log('hello', notes)
+//     return JSON.parse(notes)
+// }
 
-async function saveNotes(userNotes) {
-    await fs.promises.writeFileq('./db/db.json', JSON.stringify(userNotes, null, 2))
-    console.log('Notes Added')
-}
+// async function saveNotes(userNotes) {
+//     await fs.promises.writeFileq('./db/db.json', JSON.stringify(userNotes, null, 2))
+//     console.log('Notes Added')
+// }
 
 // Load Routes
 app.use('/', note_routes)
@@ -26,7 +26,7 @@ app.listen(PORT, () => {
     console.log('Note Taker App is running!')
 })
 
-module.exports = { 
-    getNotes,
-    saveNotes
-}
+// module.exports = { 
+//     getNotes,
+//     saveNotes
+// }
